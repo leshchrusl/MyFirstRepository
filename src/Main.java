d5a5742f98c2e37f1c024c9874543ddd004042de
ruslan <leshchinskyruslan@gmail.com>
2024-12-12 18:22:23 +0300

    change downloaded rep

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is the Main file");
+        System.out.println("This is the CHANGED Main file");
         }
     }
